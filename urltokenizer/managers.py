20e2dff37b219c7c02b4780d91377d888b628a9c
Sergio Rodriguez <sergio_mu12@hotmail.com>
2023-07-19 01:49:26 -0300

    feat: added URLTokenizerManager

---
+++ urltokenizer/managers.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+from .tokenizer import URLTokenizer
+
+
+class URLTokenizerManager:
+    def bulk_generate_tokenized_link(
+        self,
+        token_type: str | Enum | None = None,
+        path: str | None = None,
+        domain: str | None = None,
+        protocol: str | None = None,
+        port: str | None = None,
+        email_subject: str | None = None,
+        send_email: bool = False,
+    ):
+        tokenizer = URLTokenizer(token_type)
+        return tokenizer.bulk_generate_tokenized_link(
+            self,
+            path=path,
+            domain=domain,
+            protocol=protocol,
+            port=port,
+            email_subject=email_subject,
+            send_email=send_email,
+        )
